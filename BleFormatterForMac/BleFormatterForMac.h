26d73d11607bba703ed665313b9d2b3126d2cc13
ADK114019 <ADK114019@adk114019-no-imac.local>
2016-12-27 16:58:52 +0900

    Initial Commit

---
+++ BleFormatterForMac/BleFormatterForMac.h
@@ -0,0 +1,19 @@
+//
+//  BleFormatterForMac.h
+//  BleFormatterForMac
+//
+//  Created by ADK114019 on 2016/12/27.
+//  Copyright © 2016年 ADK114019. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BleFormatterForMac.
+FOUNDATION_EXPORT double BleFormatterForMacVersionNumber;
+
+//! Project version string for BleFormatterForMac.
+FOUNDATION_EXPORT const unsigned char BleFormatterForMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BleFormatterForMac/PublicHeader.h>
+
+
